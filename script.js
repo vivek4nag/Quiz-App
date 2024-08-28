275a981813cc79edbdf58fd70257982b16baa93c
viek4nag <vivek4nag@gmail.com>
2024-08-28 22:37:15 +0530

    basic quiz app

---
+++ script.js
@@ -0,0 +1,127 @@
+const questions = [
+    {
+        question: "which is the largrst animal in the world?",
+        answers: [
+            {text: "Shark", correct: false},
+            {text: "Blue Whale", correct: true},
+            {text: "Elephant", correct: false},
+            {text: "Giraffe", correct: false},
+        ]
+    },
+    {
+        question: "which is the Smallest Country in the world?",
+        answers: [
+            {text: "Vatican City", correct: true},
+            {text: "Bhutan", correct: false},
+            {text: "Nepal", correct: false},
+            {text: "Sri Lanka", correct: false},
+        ]
+    },
+    {
+        question: "which is the Largest Desert in the world?",
+        answers: [
+            {text: "Kalahari", correct: false},
+            {text: "Gobi", correct: false},
+            {text: "Sahara", correct: false},
+            {text: "Antartica", correct: true},
+        ]
+    },
+    {
+        question: "which is the Smallest Continent in the world?",
+        answers: [
+            {text: "Asia", correct: false},
+            {text: "Australia", correct: true},
+            {text: "Arctic", correct: false},
+            {text: "Africa", correct: false},
+        ]
+    }
+];
+
+const questionElement = document.getElementById("question");
+const answerButtons = document.getElementById("ans-buttons");
+const nextButton = document.getElementById("next-btn");
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+function startQuiz(){
+    currentQuestionIndex = 0;
+    score = 0;
+    nextButton.innerHTML = "Next";
+    showQuestion();
+}
+
+
+function showQuestion(){
+    //to reset prev question
+    resetState();
+
+    //1st code to show question
+    let currentQuestion = questions[currentQuestionIndex];
+    let questionNo = currentQuestionIndex + 1;
+    questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
+
+    //code to show ans
+    currentQuestion.answers.forEach(answer => {
+        const button = document.createElement("button");
+        button.innerHTML = answer.text;
+        button.classList.add("btn");
+        answerButtons.appendChild(button);
+        if(answer.correct){
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener("click", selectAnswer)
+    });
+
+}
+
+function resetState(){
+    nextButton.style.display = "none";
+    while(answerButtons.firstChild){
+        answerButtons.removeChild(answerButtons.firstChild)
+    }
+}
+
+function selectAnswer(e){
+    const selectedBtn = e.target;
+    const isCorrect = selectedBtn.dataset.correct === "true";
+    if(isCorrect){
+        selectedBtn.classList.add("correct");
+        score++;
+    }else{
+        selectedBtn.classList.add("incorrect")
+    }
+    Array.from(answerButtons.children).forEach(button => {
+        if(button.dataset.correct === "true"){
+            button.classList.add("correct");
+        }
+        button.disabled = true;
+    });
+    nextButton.style.display = "block";
+}
+
+function showScore(){
+    resetState();
+    questionElement.innerHTML = `You Scored ${score} out of ${questions.length}!`;
+    nextButton.innerHTML = "Play Again"
+    nextButton.style.display = "block"
+}
+
+function handleNextButton(){
+    currentQuestionIndex++;
+    if(currentQuestionIndex < questions.length){
+        showQuestion();
+    }else{
+        showScore();
+    }
+}
+
+nextButton.addEventListener("click", () =>{
+    if(currentQuestionIndex < questions.length){
+        handleNextButton();
+    }else{
+        startQuiz();
+    }
+})
+
+startQuiz();
